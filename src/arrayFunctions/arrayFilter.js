74b33e7456fe7eba48c4795b095a1bbfdd42441c
cristhiansoria <crisso2292@gmail.com>
2018-07-30 20:15:39 -0400

    Adding array.filter demo

--- src/arrayFunctions/arrayFilter.js
+++ src/arrayFunctions/arrayFilter.js
@@ -4,10 +4,50 @@
  * by the provided function.
  */
 
+let invalidEntries = 0;
+
+//  private methods and helpers
+const isNumber = obj => {
+  return obj !== "undefined" && typeof obj === "number" && !isNaN(obj);
+};
+
+const filterById = item => {
+  if (isNumber(item.id) && item.id !== 0) {
+    return true;
+  }
+  invalidEntries++;
+  return false;
+};
+
+// Exported Methods
+
 const getWordsGreaterThan = (words, conditional) => {
   return words.filter(word => word.length > conditional);
 };
 
+const getElmsSmallerThan = (arr, condition) => {
+  return arr.filter(each => {
+    if (each < condition) return true;
+    return false;
+  });
+};
+
+const getInvalidEntries = () => invalidEntries;
+
+const getInvalidJSONEntries = arr => {
+  return arr.filter(filterById);
+};
+
+const filterQuery = (arr, query) => {
+  return arr.filter(
+    each => each.toLowerCase().indexOf(query.toLowerCase()) > -1
+  );
+};
+
 module.exports = {
-  getWordsGreaterThan: getWordsGreaterThan
+  getWordsGreaterThan: getWordsGreaterThan,
+  getElmsSmallerThan: getElmsSmallerThan,
+  getInvalidJSONEntries: getInvalidJSONEntries,
+  getInvalidEntries: getInvalidEntries,
+  filterQuery: filterQuery
 };
--- test/arrayFunctions/arrayFilterTest.js
+++ test/arrayFunctions/arrayFilterTest.js
@@ -12,9 +12,42 @@ describe("array.filter() functionalioty", () => {
       "destruction",
       "present"
     ];
+    ``;
     let result = array.getWordsGreaterThan(words, 6);
     expect(result).to.eql(expectedResult);
   });
 
-  it("", () => {});
+  it("shoud check that we get an array with elements smaller than 20", () => {
+    const testArr = [14, 22, 56, 4, 7, 19, 20, 90];
+    const expectedResult = [14, 4, 7, 19];
+    let result = array.getElmsSmallerThan(testArr, 20);
+    expect(result).to.eql(expectedResult);
+  });
+
+  it("should check that we get invalid entries from JSON.", () => {
+    const testArr = [
+      { id: 15 },
+      { id: -1 },
+      { id: 0 },
+      { id: 3 },
+      { id: 12.2 },
+      {},
+      { id: null },
+      { id: NaN },
+      { id: "undefined" }
+    ];
+
+    const expectedResult = [{ id: 15 }, { id: -1 }, { id: 3 }, { id: 12.2 }];
+    const expectedInvalidEntries = 5;
+    let result = array.getInvalidJSONEntries(testArr);
+    expect(result).to.eql(expectedResult);
+    expect(array.getInvalidEntries()).to.eql(expectedInvalidEntries);
+  });
+
+  it("should check that it returns filtered results.", () => {
+    const testArr = ["apple", "banana", "grapes", "mango", "orange"];
+    const expectedResult = ["banana", "mango", "orange"];
+    const result = array.filterQuery(testArr, "an");
+    expect(result).to.eql(expectedResult);
+  });
 });
